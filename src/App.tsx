87cc84c7567d73a7c85c145263bd613030d7cb09
Bogdan Logojan <logojan.bogdy@gmail.com>
2025-11-13 23:29:37 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Card } from 'antd'
+import './App.css'
+
+function App() {
+
+  return (
+    <Card title="Default card" style={{ width: 300 }}>
+      <p>test card</p>
+    </Card>
+  )
+}
+
+export default App
