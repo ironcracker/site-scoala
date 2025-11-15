d17ba5f8324db0e919cad8e7856487f92e054f21
Bogdan Logojan <logojan.bogdy@gmail.com>
2025-11-15 22:16:54 +0200

    set vite config host to true

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/site-scoala/',
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
   },
   build: {
