7aa586b19425101ae0c65cc49fbc16619b690900
Bogdan Logojan <logojan.bogdy@gmail.com>
2025-11-15 21:48:45 +0200

    fix deployment server

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/site-scoala/',
   plugins: [react()],
   server: {
-    host: true,
     port: 3000,
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    },
 })
