2f0568353c8a626ddd3adb2de530fdf35d7af305
Bogdan Logojan <logojan.bogdy@gmail.com>
2025-11-17 20:45:08 +0200

    clean imports

--- src/components/Galerie.tsx
+++ src/components/Galerie.tsx
@@ -2,23 +2,23 @@ import { Flex } from 'antd'
 import {CardFoto} from './CardFoto'
 import placeholder from '../assets/test.jpg'
 import placeholder2 from '../assets/placeholder.png'
-import { Ianuarie, Februarie, Martie, Aprilie, Mai, Iunie, Iulie,August, Septembrie, Octombrie, Noiembrie, Decembrie, textIanuarie, textAprilie, textAugust, textDecembrie, textFebruarie, textIulie, textIunie, textMai, textMartie, textNoiembrie, textOctombrie, textSeptembrie } from '../helpers/text'
+import * as texte from '../helpers/text'
 
 const Galerie = () => {
     return (
         <Flex align="center" justify='center' style={{gap: '20px', padding: '20px'}} wrap>
-          <CardFoto name={Ianuarie} description={textIanuarie} imagePath={placeholder} /> 
-          <CardFoto name={Februarie} description={textFebruarie} imagePath={placeholder2} /> 
-          <CardFoto name={Martie} description={textMartie} imagePath={placeholder} /> 
-          <CardFoto name={Aprilie} description={textAprilie} imagePath={placeholder2} /> 
-          <CardFoto name={Mai} description={textMai} imagePath={placeholder} /> 
-          <CardFoto name={Iunie} description={textIunie} imagePath={placeholder} /> 
-          <CardFoto name={Iulie} description={textIulie} imagePath={placeholder2} /> 
-          <CardFoto name={August} description={textAugust} imagePath={placeholder} /> 
-          <CardFoto name={Septembrie} description={textSeptembrie} imagePath={placeholder} /> 
-          <CardFoto name={Octombrie} description={textOctombrie} imagePath={placeholder} /> 
-          <CardFoto name={Noiembrie} description={textNoiembrie} imagePath={placeholder2} /> 
-          <CardFoto name={Decembrie} description={textDecembrie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Ianuarie} description={texte.textIanuarie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Februarie} description={texte.textFebruarie} imagePath={placeholder2} /> 
+          <CardFoto name={texte.Martie} description={texte.textMartie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Aprilie} description={texte.textAprilie} imagePath={placeholder2} /> 
+          <CardFoto name={texte.Mai} description={texte.textMai} imagePath={placeholder} /> 
+          <CardFoto name={texte.Iunie} description={texte.textIunie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Iulie} description={texte.textIulie} imagePath={placeholder2} /> 
+          <CardFoto name={texte.August} description={texte.textAugust} imagePath={placeholder} /> 
+          <CardFoto name={texte.Septembrie} description={texte.textSeptembrie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Octombrie} description={texte.textOctombrie} imagePath={placeholder} /> 
+          <CardFoto name={texte.Noiembrie} description={texte.textNoiembrie} imagePath={placeholder2} /> 
+          <CardFoto name={texte.Decembrie} description={texte.textDecembrie} imagePath={placeholder} /> 
         </Flex>
     )}
 export default Galerie;
